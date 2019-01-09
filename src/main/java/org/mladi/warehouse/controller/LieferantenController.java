cf6a6d580d64ee700283de0a4597245ebfb0e2ca
DroidLock <DroidLock@users.noreply.github.com>
2019-01-09 13:48:52 +0100

    thymeleaf WIP (Dropdown working)

--- src/main/java/org/mladi/warehouse/controller/LieferantenController.java
+++ src/main/java/org/mladi/warehouse/controller/LieferantenController.java
@@ -1,28 +1,28 @@
-package org.mladi.warehouse.controller;
-
-import org.mladi.warehouse.repository.ILieferantenRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class LieferantenController {
-
-    private ILieferantenRepository iLieferantenRepository;
-
-    @Autowired
-    public LieferantenController(ILieferantenRepository iLieferantenRepository) {
-        this.iLieferantenRepository = iLieferantenRepository;
-    }
-
-    @RequestMapping("")
-    public String getLieferant(Model model){
-        model.addAttribute("lieferant", iLieferantenRepository.findAll());
-
-        return "lieferant";
-    }
-
-}
+//package org.mladi.warehouse.controller;
+//
+//import org.mladi.warehouse.repository.ILieferantenRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class LieferantenController {
+//
+//    private ILieferantenRepository iLieferantenRepository;
+//
+//    @Autowired
+//    public LieferantenController(ILieferantenRepository iLieferantenRepository) {
+//        this.iLieferantenRepository = iLieferantenRepository;
+//    }
+//
+//    @RequestMapping("/addArtikel")
+//    public String getLieferant(Model model){
+//        model.addAttribute("lieferant", iLieferantenRepository.findAll());
+//
+//        return "addArtikel";
+//    }
+//
+//}
--- src/main/java/org/mladi/warehouse/controller/addArtikelController.java
+++ src/main/java/org/mladi/warehouse/controller/addArtikelController.java
@@ -1,13 +1,27 @@
 package org.mladi.warehouse.controller;
 
+import org.mladi.warehouse.repository.ILieferantenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class addArtikelController {
 
+    private ILieferantenRepository iLieferantenRepository;
+
+    @Autowired
+    public addArtikelController(ILieferantenRepository iLieferantenRepository) {
+        this.iLieferantenRepository = iLieferantenRepository;
+    }
+
     @RequestMapping("/addArtikel")
-    public String letAddArtikel(){
+    public String letAddArtikel(Model model){
+            model.addAttribute("lieferant", iLieferantenRepository.findAll());
+
         return "addArtikel";
     }
+
+//    @RequestMapping("addArtikel")
 }
